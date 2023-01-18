3a09660706d076c02b6c1d7acb53b8a24f0b8853
jake grella <jake@jakegrella.com>
2023-01-17 20:57:26 -0600

    remove autoincrement build step for production in eas.json

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "Cloud Computing Jobs",
     description: "Explore companies and engineering jobs",
     slug: "ccj-app",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "dark",
